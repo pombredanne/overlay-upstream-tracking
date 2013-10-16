3bb3d8a63a26687ef71576fef3547c7a3ea39992
Gregory M. Turner <gmt@be-evil.net>
2013-10-16 03:37:09 -0700

    initial import (skeleton)
    Signed-off-by: Gregory M. Turner <gmt@be-evil.net>

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(name='overlay-upstream-tracking',
+      version='0.1',
+      classifiers=[
+	      'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+	      'Topic :: Software Development :: Version Control',
+	      ],
+      description='Tools to partially automate tracking of upstream ebuild/eclass code deltas in order to ease the overlay maintenace process.',
+      author='Gregory M. Turner',
+      author_email='gmt@be-evil.net',
+      packages=['overlay-upstream-tracking'],
+      url='http://fixme/',
+      scripts=['scripts/pull-upstream-tracking'],
+      )
