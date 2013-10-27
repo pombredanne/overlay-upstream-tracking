82064db1bc66e7db2ee8cc58603847e47ca792dc
Gregory M. Turner <gmt@be-evil.net>
2013-10-27 12:43:41 -0700

    Rules: hard code outrules.d location.  gnash teeth.
    Well, poop.  The setuptools way just doesn't seem to work.  Use only
    hard-coded paths and write a big complainey comment about how that's
    messing up my /feng shui/.
    
    Signed-off-by: Gregory M. Turner <gmt@be-evil.net>

--- OverlayUpstreamTracking/Rules.py
+++ OverlayUpstreamTracking/Rules.py
@@ -20,13 +20,36 @@ from pkg_resources import Requirement, resource_filename
 from portage.const import EPREFIX
 import os
 
-class FileNotFoundException(Exception):
-	'''Thrown when file is not found'''
+__all__ = [ 'GlobalRulesNotFoundException', 'get_global_rules_filepath' ]
 
+class GlobalRulesNotFoundException(Exception):
+	'''Thrown if the global rules repository is not present where OverlayUpstreamTracking.Rules
+	   expects to find it (typically: /usr/share/overlay-upstream-tracking/outrules.d)'''
+
+# FIXME: I hate that we are forced to stop pretending we are platform independent here.
+# Even though we really aren't, the core idea rules engine could pretty easily be made
+# platform independant and applied to other problem domains.  So I really wanted to avoid
+# hard-coding any platform dependencies and have been going out of my way not to.
+#
+# TODO: ask upstream setuptools folks for help with this?
+#
+# Ideally this would use something like:
+#
+#   http://pythonhosted.org/setuptools/setuptools.html#non-package-data-files
+#
+# but, sadly, the install_data behavior documented there simply does not occur or work.
+# Even if it did, that would put our files somewhere unacceptably obscure.
+# Instead we rely on the 'files go in /usr' distutils default which setuptools (erroneously?)
+# does not modify.... *confused*
+_EXPECTED_GLOBAL_OUTRULES_D_LOCATION = EPREFIX + '/usr/share/overlay-upstream-tracking/outrules.d'
+
+_global_rules_filepath = None
 def get_global_rules_filepath():
-	basepath = EPREFIX + '/usr/share/overlay-upstream-tracking/outrules.d/base';
-	if not os.path.isfile(basepath):
-		basepath = resource_filename(Requirement.parse("overlay-upstream-tracking"), "outrules.d/base")
-		if not os.path.isfile(basepath):
-			raise FileNotFoundException('outrules.d/base')
-	return os.path.dirname(basepath)
+	global _global_rules_filepath
+	if _global_rules_filepath == None:
+		# test for outrules.d/base... if it's there we'll assume everything's good.
+		testpath = _EXPECTED_GLOBAL_OUTRULES_D_LOCATION + '/base'
+		if not os.path.isfile(testpath):
+			raise FileNotFoundException('Path %s not found' % _EXPECTED_OUTRULES_D_LOCATION)
+		_global_rules_filepath = os.path.dirname(testpath)
+	return _global_rules_filepath
